4aaa80888c63c7f38f42781876ab18bc1329683c
Szigeti Máté <szigetimate@Szigeti-MacBook-Pro.local>
2024-02-16 13:49:01 +0100

    project done

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 def main():
-    book_path = "books/frankenstein.rtf"
+    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    #print(text)
+
+    sorted_list = []
+
+    with open("books/frankenstein.txt", "r") as file:
+        book_text = file.read()
+
+    letters = characters_num(book_text)
+
+    for i in letters:
+        sorted_list.append({"name": i, "num": letters.get(i)})
+    sorted_list.sort(reverse=True, key=sort_on)
+
+    print("--- Begin report of " + book_path + "---")
+    print(str(get_num_words(text)) + " words found in the document")
+    for i in sorted_list:
+        print("The '" + i["name"] + "' character was found " + str(i["num"]) + " times")
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -19,4 +36,9 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
     
+def sort_on(letters):
+    return letters["num"]
+
+
+
 main()
