dfc603acb1d8d1308c48d37be8a0610670cc937d
Szigeti Máté <szigetimate@Szigeti-MacBook-Pro.local>
2024-02-16 12:17:25 +0100

    count letters done

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-
+def characters_num(text):
+    lower_case_text = text.lower()
+    letters = {}
+    for char in lower_case_text:
+        if char.isalpha():
+            letters[char] = letters.get(char, 0) + 1
+    return letters
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
     
-
+main()
