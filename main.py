2bf445b3e8a82422af383288745cd3cf830d60cd
Szigeti Máté <szigetimate@Szigeti-MacBook-Pro.local>
2024-02-16 11:50:25 +0100

    i am stopping at part 15 of the course, at count letters chapter

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    book_path = "books/frankenstein.rtf"
+    text = get_book_text(book_path)
+    print(text)
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+
